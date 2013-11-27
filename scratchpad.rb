0c62fe2c229acecf798c4b6737a81c69096fde25
zumbalogy <intrapostal@gmail.com>
2013-11-27 15:41:58 -0500

    Update scratchpad.rb

--- scratchpad.rb
+++ scratchpad.rb
@@ -3,7 +3,6 @@
 
 
 
-
 def draw_line input
     7.times do |index|
         shift = index + 1
@@ -20,3 +19,49 @@ end
 
 
 #do some proc thing here to yeald to what the first half that if should be
+
+if row != 7
+    7.times do |index|
+        shift = index + 1
+        if $board[row + shift][col] == bad
+            $board[row + shift][col] = (shift + 3)
+        else
+            break
+        end
+    end
+    line_confirm good, bad
+end
+
+7.times do |index|
+    7.times do |index2|
+        shift = index2 + 1
+        if index == 0
+            a = row - shift
+            b = col
+        elsif index == 1
+            a = row
+            b = col + shift
+        elsif index == 2
+            a = row
+            b = col -shift
+        elsif index == 3
+            a = row + shift
+            b = col + shift
+        elsif index == 4
+            a = row - shift
+            b = col - shift
+        elsif index == 5
+            a = row - shift
+            b = col + shift
+        elsif index == 6
+            a = row + shift
+            b = col - shift
+        end
+        if $board[a][b] == bad
+            $board[a][b] = (shift + 3)
+        else
+            break
+        end
+        line_confirm good, bad
+    end       
+end
