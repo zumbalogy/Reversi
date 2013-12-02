7626047cf81e3d310aea3a4524c1b8a037300532
zumbalogy <intrapostal@gmail.com>
2013-12-02 17:36:53 -0500

    shorter line checking

--- reversi.rb
+++ reversi.rb
@@ -135,11 +135,12 @@ def move good, bad, input
         if $board[row][col] == -1
             $board[row][col] = 3
 
+                        
             if row != 7
                 7.times do |index|
                     shift = index + 1
                     if $board[row + shift][col] == bad
-                        $board[row + shift][col] = (index + 3)
+                        $board[row + shift][col] = (shift + 3)
                     else
                         break
                     end
@@ -148,74 +149,39 @@ def move good, bad, input
             end
 
             7.times do |index|
-                shift = index + 1
-                if $board[row - shift][col] == bad
-                    $board[row - shift][col] = (shift + 3)
-                else
-                    break
-                end
-            line_confirm good, bad
+                7.times do |index2|
+                    shift = index2 + 1
+                    if index == 0
+                        a = row - shift
+                        b = col
+                    elsif index == 1
+                        a = row
+                        b = col + shift
+                    elsif index == 2
+                        a = row
+                        b = col -shift
+                    elsif index == 3
+                        a = row + shift
+                        b = col + shift
+                    elsif index == 4
+                        a = row - shift
+                        b = col - shift
+                    elsif index == 5
+                        a = row - shift
+                        b = col + shift
+                    elsif index == 6
+                        a = row + shift
+                        b = col - shift
+                    end
+                    if $board[a][b] == bad
+                        $board[a][b] = (shift + 3)
+                    else
+                        break
+                    end
+                    line_confirm good, bad
+                end       
             end
 
-            7.times do |index|
-                shift = index + 1
-                if $board[row][col + shift] == bad
-                    $board[row ][col + shift] = (shift + 3)
-                else
-                    break
-                end
-                line_confirm good, bad
-            end
-
-            7.times do |index|
-                shift = index + 1
-                if $board[row][col - shift] == bad
-                    $board[row ][col - shift] = (shift + 3)
-                else
-                    break
-                end
-                line_confirm good, bad
-            end
-
-            7.times do |index|
-                shift = index + 1
-                if $board[row + shift][col + shift] == bad
-                    $board[row + shift][col + shift] = (shift + 3)
-                else
-                    break
-                end
-                line_confirm good, bad
-            end
-
-            7.times do |index|
-                shift = index + 1
-                if $board[row - shift][col - shift] == bad
-                    $board[row - shift][col - shift] = (shift + 3)
-                else
-                    break
-                end
-                line_confirm good, bad
-            end
-
-            7.times do |index|
-                shift = index + 1
-                if $board[row - shift][col + shift] == bad
-                    $board[row - shift][col + shift] = (shift + 3)
-                else
-                    break
-                end
-                line_confirm good, bad
-            end
-
-            7.times do |index|
-                shift = index + 1
-                if $board[row + shift][col - shift] == bad
-                    $board[row + shift][col - shift] = (shift + 3)
-                else
-                    break
-                end
-                line_confirm good, bad
-            end
 
             $board.each_with_index do |row, row_num|
                 row.each_with_index do |cell, cell_num|
